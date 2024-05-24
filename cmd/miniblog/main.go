a86eddfc40e2b255b6fa98380b85d7e1c3bc4bf1
Jingfeng Du <jeffduuu@gmail.com>
2024-05-24 17:36:10 +0800

    feat: add viper

--- cmd/miniblog/main.go
+++ cmd/miniblog/main.go
@@ -8,8 +8,7 @@ package main
 import (
 	"github.com/ra1n6ow/miniblog/internal/miniblog"
 	// 通过导入匿名包 go.uber.org/automaxprocs 来使程序自动设置 GOMAXPROCS 以匹配 Linux 容器 CPU 配额。
-	//  通过正确设置容器的 CPU 配额，可以解决 GOMAXPROCS 可能设置过大，导致生成线程过多，从而导致严重的上下文切换，
-	// 浪费 CPU，降低程序性能的潜在问题。
+	//  通过正确设置容器的 CPU 配额，可以解决 GOMAXPROCS 可能设置过大，导致生成线程过多，从而导致严重的上下文切换，浪费 CPU，降低程序性能的潜在问题。
 	// 参考1：https://pandaychen.github.io/2020/02/28/GOMAXPROCS-POT/
 	// 参考2：https://pandaychen.github.io/2020/02/29/AUTOMAXPROCS-ANALYSIS/
 	//_ "go.uber.org/automaxprocs"
