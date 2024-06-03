d016ece3338fa54d277cc01ccae9c0d688e5ed5b
Jingfeng Du <jeffduuu@gmail.com>
2024-06-03 12:59:24 +0800

    feat: tls

--- pkg/auth/authz.go
+++ pkg/auth/authz.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2024 ra1n6ow <jeffduuu@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/ra1n6ow/miniblog.
+
 package auth
 
 import (
